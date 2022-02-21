9c623b3b1fcf6df93329832e3c2e4a0e1bd63e6e
rafipera <ersozlu.rafi@gmail.com>
2022-02-21 03:51:56 +0300

    completed
    completed gun mode, help button and a modal added.

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
+//author Rafi Ersozlu github.com/rafipera
+
 const btn = document.getElementById("btn")
 const grid = document.querySelector(".grid")
 let score = document.getElementById("score")
 const highScore = document.getElementById("high-score")
 const hardBtn = document.getElementById("hard-btn")
+const helpBtn = document.getElementById("help")
+const modal = document.querySelector(".modal")
+let ammoDirection = 0
+let helpBtnReady = true
+let ammoReady = true
+let movementReady = true
 const size = 400
 let squares = []
 let snake = [2,1,0]
 let direction = 1
-width = 20
+let width = 20
 let timerId = 0
 let speed = 400
 let apple = 0
 let scoreDisp = 0
+let ammo = 0
+let ammoTimer = 0
+let mode = ""
+let tail = snake[2]
 createTable()
 snake.forEach(i => squares[i].classList.add("snake"))
 highScore.innerHTML = localStorage.getItem("score")
@@ -20,12 +32,17 @@ if(localStorage.getItem("score")==null){
     localStorage.setItem("score",0)
 }
 
+
+//MOVEMENT OF SNAKE & AMMO
+
 function move(){
     if(
         (snake[0] % width === 0 && direction === -1) ||
         (snake[0] % width === width - 1 && direction === 1)||
         (snake[0] + width >= width*width  && direction == width)||
-        (snake[0] - width < 0) && direction === -width){
+        (snake[0] - width < 0 && direction === -width) ||
+        (squares[snake[0]+direction].classList.contains("snake"))
+        ){
         score.innerHTML = `Game Over with "${scoreDisp}" points`
         if(parseInt(localStorage.getItem("score")) < scoreDisp){
             localStorage.setItem("score", scoreDisp)
@@ -33,7 +50,7 @@ function move(){
         }
         return clearInterval(timerId)
     }
-    const tail = snake.pop()
+    tail = snake.pop()
     squares[tail].classList.remove("snake")
     snake.unshift(snake[0]+direction)
     squares[snake[0]].classList.add("snake")
@@ -54,9 +71,93 @@ function move(){
         }
     }
 
+    movementReady = true
+
 }
 
+function moveHard(){
+    if(
+        (snake[0] % width === 0 && direction === -1) ||
+        (snake[0] % width === width - 1 && direction === 1)||
+        (snake[0] + width >= width*width  && direction == width)||
+        (snake[0] - width < 0 && direction === -width) ||
+        (squares[snake[0]+direction].classList.contains("snake"))
+        ){
+        score.innerHTML = `Game Over with "${scoreDisp}" points`
+        if(parseInt(localStorage.getItem("score")) < scoreDisp){
+            localStorage.setItem("score", scoreDisp)
+            highScore.textContent = localStorage.getItem("score")
+        }
+        return clearInterval(timerId)
+    }
+    tail = snake.pop()
+    squares[tail].classList.remove("snake")
+    snake.unshift(snake[0]+direction)
+    squares[snake[0]].classList.add("snake")
 
+    if(snake[0] === apple){
+        squares[apple].classList.remove("apple")
+        scoreDisp += 1
+        score.innerHTML = scoreDisp
+        generateApple()
+        snake.push(tail)
+        squares[tail].classList.add("snake")
+        clearInterval(timerId)
+        speed = speed * 0.9
+        timerId = setInterval(moveHard, speed)
+        if(parseInt(localStorage.getItem("score")) < scoreDisp){
+            localStorage.setItem("score", scoreDisp)
+            highScore.textContent = localStorage.getItem("score")
+        }
+    }
+
+
+    movementReady = true
+
+}
+
+function ammoShot(){
+    if(
+        (ammo % width === 0 && ammoDirection === -1)||
+        (ammo % width === width-1 && ammoDirection === 1)||
+        (ammo + width >= width*width && ammoDirection == width)||
+        (ammo - width < 0 && ammoDirection === -width)
+        ){
+        ammoReady= true
+        squares[ammo].classList.remove("ammo")
+        squares[ammo].innerHTML = "" //!!!
+        return clearInterval(ammoTimer)
+    }
+    squares[ammo].classList.remove("ammo")
+    squares[ammo].innerHTML = "" //!!!
+    ammo += ammoDirection
+    squares[ammo].classList.add("ammo")
+    squares[ammo].innerHTML = "•"
+
+    if(ammo === apple){
+        squares[apple].classList.remove("apple")
+        squares[ammo].classList.remove("ammo")
+        squares[ammo].innerHTML = "" //!!!
+        scoreDisp += 1
+        score.innerHTML = scoreDisp
+        generateApple()
+        snake.push(tail)
+        squares[tail].classList.add("snake")
+        clearInterval(timerId)
+        clearInterval(ammoTimer)
+        speed = speed * 0.9
+        timerId = setInterval(moveHard, speed)
+        if(parseInt(localStorage.getItem("score")) < scoreDisp){
+            localStorage.setItem("score", scoreDisp)
+            highScore.textContent = localStorage.getItem("score")
+        }
+        ammoReady = true
+
+    }
+
+}
+
+//GENERATING APPLE
 
 function generateApple(){
     
@@ -69,37 +170,17 @@ function generateApple(){
 
 }
 
-
-document.addEventListener("keydown", (e) => {
-    if(e.key === "ArrowRight"){
-        if(direction != -1){
-            direction = 1
-        }
-
-    }else if(e.key === "ArrowLeft"){
-        if(direction != 1){
-            direction = -1
-        }
-        
-    }else if(e.key==="ArrowDown"){
-        if(direction != -width){
-            direction = width
-        }
-    }else if(e.key ==="ArrowUp"){
-        if(direction != width){
-            direction = -width
-        }
-    }
-
-})
+//BUTTON FUNCTIONS AND TABLE
 
 function start(){
+    mode = "standart"
     clearInterval(timerId)
     scoreDisp = 0
-    speed = 500
+    speed = 600
     snake.forEach(i => squares[i].classList.remove("snake"))
     squares[apple].classList.remove("apple")
     snake = [2,1,0]
+    tail = snake[2]
     direction = 1
     snake.forEach(i => squares[i].classList.add("snake"))
     generateApple()
@@ -109,17 +190,19 @@ function start(){
 }
 
 function startHard(){
+    mode = "hard"
     clearInterval(timerId)
     scoreDisp = 0
-    speed = 200
+    speed = 500
     snake.forEach(i => squares[i].classList.remove("snake"))
     squares[apple].classList.remove("apple")
     snake = [2,1,0]
+    tail = snake[2]
     direction = 1
     snake.forEach(i => squares[i].classList.add("snake"))
     generateApple()
     score.innerHTML = scoreDisp
-    timerId = setInterval(move, speed)
+    timerId = setInterval(moveHard, speed)
 
 }
 
@@ -133,6 +216,58 @@ function createTable(){
     }
 }
 
+//EVENT LISTENERS
+
 btn.addEventListener("click", start)
 
 hardBtn.addEventListener("click", startHard)
+
+
+
+helpBtn.addEventListener("click", ()=>{
+    if (helpBtnReady){
+        modal.style.display = "block"
+    }else{
+        modal.style.display = "none"        
+    }
+    helpBtnReady = !helpBtnReady
+})
+
+
+document.addEventListener("keydown", (e) => {
+    if(e.key === "f" && mode === "hard" && ammoReady){
+        ammoDirection = JSON.parse(JSON.stringify(direction))
+        ammo = JSON.parse(JSON.stringify(snake[0]+ammoDirection))
+        clearInterval(ammoTimer)
+        ammoTimer = setInterval(ammoShot,speed/6)
+        ammoReady=false
+    }
+
+})
+
+
+document.addEventListener("keydown", (e) => {
+    if(movementReady){
+        if(e.key === "ArrowRight"){
+            if(direction != -1){
+                direction = 1
+            }
+    
+        }else if(e.key === "ArrowLeft"){
+            if(direction != 1){
+                direction = -1
+            }
+            
+        }else if(e.key==="ArrowDown"){
+            if(direction != -width){
+                direction = width
+            }
+        }else if(e.key ==="ArrowUp"){
+            if(direction != width){
+                direction = -width
+            }
+        }
+        movementReady = false
+    }
+
+})
